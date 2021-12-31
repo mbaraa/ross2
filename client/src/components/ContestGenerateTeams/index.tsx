3361c75502e234cbb30293b4d3d408dd46282952
Baraa Al-Masri <m.baraa.masri@gmail.com>
2021-12-31 10:03:17 +0000

    "finished" generate teams

--- client/src/components/ContestGenerateTeams/index.tsx
+++ client/src/components/ContestGenerateTeams/index.tsx
@@ -1,19 +1,36 @@
 import Button from "../Button";
 import * as React from "react";
-import Dialog from "@mui/material/Dialog";
+import { Dialog, Input, TextField } from "@mui/material";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Title from "../Title";
 import Dropdown from "../Dropdown";
+import OrganizerRequests from "../../utils/requests/OrganizerRequests";
+import Contest from "../../models/Contest";
+import Team from "../../models/Team";
+import Contestant from "../../models/Contestant";
 
 interface Props {
   id: number;
 }
 
 const ContestGenerateTeams = ({ id }: Props) => {
+  const [contest, setContest] = React.useState<Contest>(new Contest());
+
+  React.useEffect(() => {
+    (async () => {
+      const c = await Contest.getContestFromServer(id);
+      setContest(c);
+    })();
+  }, []);
+
+  const [loading, setLoadin] = React.useState<boolean>(false);
+
   const [open, setOpen] = React.useState(false);
   const [selectedType, setSelectedType] = React.useState("ordered");
+
+  const [useGivenNames, setUseGivenNames] = React.useState<boolean>(false);
 
   let typesOfGenerate = [
     { id: 1, value: "ordered", name: "Numbered" },
@@ -29,6 +46,73 @@ const ContestGenerateTeams = ({ id }: Props) => {
     setOpen(false);
   };
 
+  const [noTeamless, setNoTeamless] = React.useState<boolean>(false);
+  const [generated, setGenerated] = React.useState<boolean>(false);
+
+  const [namesFile, setNamesFile] = React.useState<any>(undefined);
+
+  // const upload = () => {
+  //   if (document != null && document != undefined) {
+  //     setNamesFile(document?.getElementById("names").files[0]);
+  //     if (namesFile.type != "text/plain") {
+  //       window.alert("file must be of text type!");
+  //       setNamesFile(undefined);
+  //     }
+  //   }
+  // };
+  interface State {
+    names: string;
+  }
+  const [state, setState] = React.useState<any>({
+    names: "",
+  });
+
+  const readNamesFile = (): string[] => {
+    return state.names.replace("\n", "").split(",");
+    // return (await namesFile.text()).replace("\n", "").split(",");
+  };
+
+  const checkNamesFile = (): boolean => {
+    return (
+      selectedType != "given" ||
+      (selectedType == "given" && namesFile !== undefined)
+    );
+  };
+
+  const [genTeams, setGenTeams] = React.useState<Team[]>([]);
+  const [teamless, setTeamless] = React.useState<Contestant[]>([]);
+
+  const generateTeams = async () => {
+    setLoadin(true);
+
+    const [generatedTeams, leftTeamless] =
+      await OrganizerRequests.generateTeams(
+        contest,
+        selectedType,
+        readNamesFile()
+      );
+
+    if (generateTeams.length == 0 && leftTeamless == null) {
+      setNoTeamless(true);
+      return;
+    }
+
+    setGenerated(true);
+    setNoTeamless(false);
+    setLoadin(false);
+    setGenTeams(generatedTeams);
+    setTeamless(leftTeamless);
+    setOpen(false);
+
+    // console.log("gen", generatedTeams);
+    // console.log("tl", leftTeamless);
+  };
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setState({ ...state, [prop]: event.target.value });
+    };
+
   return (
     <div>
       <Button
@@ -42,7 +126,7 @@ const ContestGenerateTeams = ({ id }: Props) => {
           <div className="mb-[28px]">
             <Title
               className="text-[18px] font-[400] mb-[16px]"
-              content="Select The Way Of Generate"
+              content="Select The Way Of Generation"
             />
 
             <Dropdown
@@ -56,13 +140,32 @@ const ContestGenerateTeams = ({ id }: Props) => {
             />
           </div>
 
+          {selectedType === "given" && (
+            <>
+              {/* <Input type="file" onClick={upload} id="names"/> */}
+              past a list of the wanted teams names{" "}
+              <b>separated by a comma(,)</b>
+              <br />
+              eg: name1,name2,name3...
+              <br />
+              <TextField
+                label="Teams names list"
+                required
+                value={state.names}
+                onChange={handleChange("names")}
+              />
+              <br />
+              <br />
+            </>
+          )}
+
           <div className=" space-x-[4px] float-right">
             <Button
               className="border-[#FB4646] text-[#FB4646] hover:bg-[#FB4646]"
               content="Cancel"
               onClick={closeHandler}
             />
-            <Button className="" content="Generate" onClick={closeHandler} />
+            <Button className="" content="Generate" onClick={generateTeams} />
           </div>
         </div>
       </Dialog>
