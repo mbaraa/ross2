ccb28c0242f7dfbae09dfecb7af91079b6cedad2
“Akram <al.awad7074@gmail.com>
2021-12-31 15:07:08 +0200

    profile page

--- client/pages/profile/index.tsx
+++ client/pages/profile/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Login from "../../src/components/Login";
+import Contestant from "../../src/models/Contestant";
 import User, { UserType } from "../../src/models/User";
+import ContestantRequests from "../../src/utils/requests/ContestantRequests";
 import MicrosoftLogin from "../../src/utils/requests/MicrosoftLogin";
 
 interface Props {
@@ -15,45 +17,40 @@ const Profile = ({ user }: Props) => {
     team: { id: 1, name: "Team 1" },
   };
 
-<<<<<<< HEAD
-  if (u === 0) {
-    return <div>Loading</div>;
-  } else if (u === null) {
-    return <div>you should login first</div>;
-  } else if ((u.user_type_base & UserType.Contestant) !== 0) {
-    return (
-      <div className="flex justify-center items-center">
-        <div className="border-[1px] border-[#eee] p-[16px] mb-[8px] rounded-[8px] w-[348px]">
-          <div className="text-[13px] text-[#425CBA] space-y-[4px]">
-            Your Name: {u.name}
-          </div>
-        </div>
+  const [cont, setCont] = React.useState<any>(0);
 
-        <div className="border-[1px] border-[#eee] p-[16px] mb-[8px] rounded-[8px] w-[348px]">
-          <div className="text-[13px] text-[#425CBA] space-y-[4px]">
-            Team Name: {u.team.name}
-          </div>
-        </div>
-      </div>
-    );
+  React.useEffect(() => {
+    (async () => {
+      const c = await ContestantRequests.getProfile();
+      setCont(c);
+    })();
+  }, []);
+
+  if (user === 0) {
+    return <div>Loading</div>;
+  } else if (user === null) {
+    return <div>you should login first</div>;
   }
 
-  return <div>hey {u.name}</div>;
-=======
-const index = (): React.ReactElement => {
-    // const [user, setUser] = React.useState<User>(new User());
-    // React.useEffect(() => {
-    //     login();
-    // }, []);
-    // const login = async () => {
-    //     const u = await MicrosoftLogin.loginWithToken();
-    //     setUser(u);
-    // };
+  return (
+    <div className="flex justify-center items-center">
+      <div className=" grid grid-cols-1">
+        <div className="border-[1px] border-[#eee] p-[16px] mb-[8px] rounded-[8px] w-[348px]">
+          <div className="text-[13px] text-[#425CBA] space-y-[4px]">
+            Your Name: {user.name}
+          </div>
+        </div>
 
-    return (<div className="font-[Poppins] absolute left-[0.2rem] top-[1.2em] w-full">
-        <Login/> 
-    </div>);
->>>>>>> c34836f0c50c86ab867bd1378a1ccd4bff67ae42
+        {cont !== 0 && cont.team.name !== "" && (
+          <div className="border-[1px] border-[#eee] p-[16px] mb-[8px] rounded-[8px] w-[348px]">
+            <div className="text-[13px] text-[#425CBA] space-y-[4px]">
+              Team Name: {cont.team.name}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
