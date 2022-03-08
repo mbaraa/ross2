eed9893b4728ca7afd2a019a4c5ae2073621abae
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-03-08 18:02:24 +0300

    🛠 stupid permissions

--- client/src/components/Organizer/CreateEditContest/index.tsx
+++ client/src/components/Organizer/CreateEditContest/index.tsx
@@ -17,6 +17,7 @@ import ImageUploader from "../../Shared/ImageUploader";
 import config from "../../../config";
 import OrganizerRequests from "../../../utils/requests/OrganizerRequests";
 import Title from "../../Shared/Title";
+import Orgnizer, { OrganizerRole } from "../../../models/Organizer";
 
 interface LabelProps {
   text: string;
@@ -202,197 +203,208 @@ const CreateEditContest = ({ user, contest }: Props): React.ReactElement => {
     })();
   };
 
-  if (
-    !checkUserType(user, UserType.Director) &&
-    !checkUserType(user, UserType.Admin)
-  ) {
-    return (
-      <Title
-        className="mb-[8px]"
-        content="Hmm... I don't think you can do that!"
-      />
-    );
-  }
+  const [canEdit, setCanEdit] = React.useState(false);
+  React.useEffect(() => {
+    (async () => {
+      const org = await OrganizerRequests.getProfile();
+      setCanEdit(
+        await OrganizerRequests.checkOrgRole(
+          contest2.id,
+          org.id,
+          OrganizerRole.Director
+        )
+      );
+    })();
+  }, []);
 
   return (
-    <div className="grid md:grid-cols-2 grid-cols-1">
-      {/* left side */}
-      <div>
-        {!isEdit && (
-          <h1 className="font-Ropa text-[30px] text-ross2">New Contest</h1>
-        )}
-        {isEdit && (
-          <>
+    <>
+      {canEdit ? (
+        <div className="grid md:grid-cols-2 grid-cols-1">
+          {/* left side */}
+          <div>
+            {!isEdit && (
+              <h1 className="font-Ropa text-[30px] text-ross2">New Contest</h1>
+            )}
+            {isEdit && (
+              <>
+                <Button
+                  startIcon={<MdDelete size={12} />}
+                  color="error"
+                  variant="outlined"
+                  size="large"
+                  onClick={deleteContest}
+                >
+                  <label className="normal-case font-Ropa cursor-pointer">
+                    Delete Contest
+                  </label>
+                </Button>
+              </>
+            )}
+            <div className="grid sm:grid-cols-2 grid-cols-1 pt-[20px]">
+              {/* inner left side */}
+              <div className="pr-[25px]">
+                <TextField
+                  className="w-[100%]"
+                  variant="outlined"
+                  value={contest2.name}
+                  onChange={handleChange("name")}
+                  label={<FieldLabel text="Contest Title" />}
+                />
+                <div className="pt-[25px]" />
+                <TextField
+                  className="w-[100%]"
+                  variant="outlined"
+                  value={contest2.location}
+                  onChange={handleChange("location")}
+                  label={<FieldLabel text="Contest Location" />}
+                />
+                <div className="pt-[15px]" />
+                <label className="font-Ropa text-[20px] text-indigo">
+                  Teams Visibility
+                </label>
+                <RadioGroup
+                  row
+                  aria-label="gender"
+                  value={contest2.teams_hidden}
+                  onChange={handleChange("teams_hidden")}
+                >
+                  <FormControlLabel
+                    value="false"
+                    control={<Radio />}
+                    label={<FieldLabel text="Visible" />}
+                  />
+                  <FormControlLabel
+                    value="true"
+                    control={<Radio />}
+                    label={<FieldLabel text="Hidden" />}
+                  />
+                </RadioGroup>
+                <div className="pt-[19px]" />
+
+                <TextField
+                  className="w-[100%]"
+                  variant="outlined"
+                  value={partConds.min_team_members}
+                  onChange={handlePartCondsChange("min_team_members")}
+                  label={<FieldLabel text="Min Members Per Team" />}
+                  type="number"
+                />
+
+                <div className="pt-[25px]" />
+
+                <TextField
+                  className="sm:w-[209%] w-[100%] h-[130px] row-span-4 col-span-4"
+                  multiline
+                  rows={4}
+                  value={contest2.description}
+                  onChange={handleChange("description")}
+                  label={<FieldLabel text="Description" />}
+                />
+
+                <div className="pt-[25px]" />
+              </div>
+
+              {/* inner right side */}
+
+              <div className="pr-[25px]">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DateTimePicker
+                    renderInput={(props) => (
+                      <TextField
+                        className="w-[100%]"
+                        variant="outlined"
+                        {...props}
+                      />
+                    )}
+                    label={<FieldLabel text="Rigistration End Date" />}
+                    value={registrationEnds}
+                    onChange={(newValue) => {
+                      setRegistrationEnds(newValue as Date);
+                      setIsModified(true);
+                    }}
+                  />
+                </LocalizationProvider>
+
+                <div className="pt-[25px]" />
+
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DateTimePicker
+                    renderInput={(props) => (
+                      <TextField
+                        className="w-[100%]"
+                        variant="outlined"
+                        {...props}
+                      />
+                    )}
+                    label={<FieldLabel text="Start Date" />}
+                    value={startsAt}
+                    onChange={(newValue) => {
+                      setStartsAt(newValue as Date);
+                      setIsModified(true);
+                    }}
+                  />
+                </LocalizationProvider>
+
+                <div className="pt-[25px]" />
+
+                <TextField
+                  className="w-[100%]"
+                  variant="outlined"
+                  value={contest2.duration}
+                  onChange={handleChange("duration")}
+                  label={<FieldLabel text="Duration (in minutes)" />}
+                  type="number"
+                />
+                <div className="pt-[25px]" />
+
+                <TextField
+                  className="w-[100%]"
+                  variant="outlined"
+                  value={partConds.max_team_members}
+                  onChange={handlePartCondsChange("max_team_members")}
+                  label={<FieldLabel text="Max Members Per Team" />}
+                  type="number"
+                />
+
+                <div className="pt-[25px]" />
+              </div>
+            </div>
+          </div>
+
+          {/* right side */}
+          <div className="sm:pt-[60px]">
+            <div onClick={() => setIsModified(true)}>
+              <ImageUploader
+                maxSize={2560}
+                imageFile={logoFile}
+                setImageFile={setLogoFile}
+                imageURL={`${config.backendAddress}${contest2.logo_path}`}
+              />
+            </div>
             <Button
-              startIcon={<MdDelete size={12} />}
-              color="error"
               variant="outlined"
-              size="large"
-              onClick={deleteContest}
+              color={isEdit ? "secondary" : "error"}
+              className="float-right"
+              startIcon={isEdit ? <MdSave size={18} /> : <GoPlus size={18} />}
+              disabled={isEdit && !isModified}
+              onClick={() => {
+                isEdit ? updateContest() : createContest();
+              }}
             >
-              <label className="normal-case font-Ropa cursor-pointer">
-                Delete Contest
+              <label className="normal-case font-Ropa text-[20px] cursor-pointer">
+                {isEdit ? "Save" : "Create"}
               </label>
             </Button>
-          </>
-        )}
-        <div className="grid sm:grid-cols-2 grid-cols-1 pt-[20px]">
-          {/* inner left side */}
-          <div className="pr-[25px]">
-            <TextField
-              className="w-[100%]"
-              variant="outlined"
-              value={contest2.name}
-              onChange={handleChange("name")}
-              label={<FieldLabel text="Contest Title" />}
-            />
-            <div className="pt-[25px]" />
-            <TextField
-              className="w-[100%]"
-              variant="outlined"
-              value={contest2.location}
-              onChange={handleChange("location")}
-              label={<FieldLabel text="Contest Location" />}
-            />
-            <div className="pt-[15px]" />
-            <label className="font-Ropa text-[20px] text-indigo">
-              Teams Visibility
-            </label>
-            <RadioGroup
-              row
-              aria-label="gender"
-              value={contest2.teams_hidden}
-              onChange={handleChange("teams_hidden")}
-            >
-              <FormControlLabel
-                value="false"
-                control={<Radio />}
-                label={<FieldLabel text="Visible" />}
-              />
-              <FormControlLabel
-                value="true"
-                control={<Radio />}
-                label={<FieldLabel text="Hidden" />}
-              />
-            </RadioGroup>
-            <div className="pt-[19px]" />
-
-            <TextField
-              className="w-[100%]"
-              variant="outlined"
-              value={partConds.min_team_members}
-              onChange={handlePartCondsChange("min_team_members")}
-              label={<FieldLabel text="Min Members Per Team" />}
-              type="number"
-            />
-
-            <div className="pt-[25px]" />
-
-            <TextField
-              className="sm:w-[209%] w-[100%] h-[130px] row-span-4 col-span-4"
-              multiline
-              rows={4}
-              value={contest2.description}
-              onChange={handleChange("description")}
-              label={<FieldLabel text="Description" />}
-            />
-
-            <div className="pt-[25px]" />
-          </div>
-
-          {/* inner right side */}
-
-          <div className="pr-[25px]">
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DateTimePicker
-                renderInput={(props) => (
-                  <TextField
-                    className="w-[100%]"
-                    variant="outlined"
-                    {...props}
-                  />
-                )}
-                label={<FieldLabel text="Rigistration End Date" />}
-                value={registrationEnds}
-                onChange={(newValue) => {
-                  setRegistrationEnds(newValue as Date);
-                  setIsModified(true);
-                }}
-              />
-            </LocalizationProvider>
-
-            <div className="pt-[25px]" />
-
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DateTimePicker
-                renderInput={(props) => (
-                  <TextField
-                    className="w-[100%]"
-                    variant="outlined"
-                    {...props}
-                  />
-                )}
-                label={<FieldLabel text="Start Date" />}
-                value={startsAt}
-                onChange={(newValue) => {
-                  setStartsAt(newValue as Date);
-                  setIsModified(true);
-                }}
-              />
-            </LocalizationProvider>
-
-            <div className="pt-[25px]" />
-
-            <TextField
-              className="w-[100%]"
-              variant="outlined"
-              value={contest2.duration}
-              onChange={handleChange("duration")}
-              label={<FieldLabel text="Duration (in minutes)" />}
-              type="number"
-            />
-            <div className="pt-[25px]" />
-
-            <TextField
-              className="w-[100%]"
-              variant="outlined"
-              value={partConds.max_team_members}
-              onChange={handlePartCondsChange("max_team_members")}
-              label={<FieldLabel text="Max Members Per Team" />}
-              type="number"
-            />
-
-            <div className="pt-[25px]" />
           </div>
         </div>
-      </div>
-
-      {/* right side */}
-      <div className="sm:pt-[60px]">
-        <div onClick={() => setIsModified(true)}>
-          <ImageUploader
-            maxSize={2560}
-            imageFile={logoFile}
-            setImageFile={setLogoFile}
-            imageURL={`${config.backendAddress}${contest2.logo_path}`}
-          />
-        </div>
-        <Button
-          variant="outlined"
-          color={isEdit ? "secondary" : "error"}
-          className="float-right"
-          startIcon={isEdit ? <MdSave size={18} /> : <GoPlus size={18} />}
-          disabled={isEdit && !isModified}
-          onClick={() => {
-            isEdit ? updateContest() : createContest();
-          }}
-        >
-          <label className="normal-case font-Ropa text-[20px] cursor-pointer">
-            {isEdit ? "Save" : "Create"}
-          </label>
-        </Button>
-      </div>
-    </div>
+      ) : (
+        <Title
+          className="mb-[8px]"
+          content="Hmm... I don't think you can do that!"
+        />
+      )}
+    </>
   );
 };
 
